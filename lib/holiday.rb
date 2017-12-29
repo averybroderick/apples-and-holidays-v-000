24e2b165cdac0e663a4d4957b4a2741625cace86
Avery Broderick <avery.c.broderick@gmail.com>
2017-12-29 19:34:37 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -83,7 +83,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, supplies|
         puts "#{season.capitalize}:"
-        puts "  #{holiday.to_s.split("_").join.capitalize}: #{supplies.join(", ")}"
+        puts "  #{holiday.to_s.split("_").capitalize.join()}: #{supplies.join(", ")}"
     end
   end
 
